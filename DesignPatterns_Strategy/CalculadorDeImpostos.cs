53a4cfed56bce38b147a81c8b805bab56e83012c
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-11 18:11:06 -0200

    Subindo o projeto

---
+++ DesignPatterns_Strategy/CalculadorDeImpostos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void Calcula(Orcamento orcamento, Imposto estrategiaDeImposto)
+        {
+            double resultado = estrategiaDeImposto.Calcula(orcamento);
+            Console.WriteLine(resultado);
+        }
+
+    }
+}
